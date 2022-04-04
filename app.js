8fdbca6a0fbc751db91fe76bfeec8f8aaf62f876
Ricardo José Nunes <ricardo.nunes@compasso.com.br>
2022-04-04 00:44:18 -0300

    Simple pagination.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -10,16 +11,33 @@ const monitoringData = require('./monitoringData.json')
 
 
 const server = http.createServer((req, res) => {
-  let url = req.url;
+  let urlObject = req.url;
+  const queryObject = url.parse(urlObject, true).query;
+
   res.statusCode = 200;
-  if (url === '/monitoring') {
+  if (urlObject.includes('/monitoring')) {
     res.setHeader('Content-Type', 'application/json');
+
+    let start = 0;
+    let end = 99;
+
+    if (typeof queryObject.start !== 'undefined' && (queryObject.start > 0 && queryObject.start < 99)) {
+      start = queryObject.start;
+    }
+    if (typeof queryObject.end !== 'undefined' && (queryObject.end > 0 && queryObject.end < 99)) {
+      end = queryObject.end;
+    }
+
+    console.log("Getting results from: " + start + " to: " + end);
+
     res.end(
-      JSON.stringify(monitoringData)
+      JSON.stringify(monitoringData.slice(start, end))
     );
   } else {
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
     res.write('Hello World!');
+    res.write('</br>');
+    res.write(`Click <a href="http://${hostname}:${port}/monitoring?start=0&end=10">here to get data!`);
     res.end();
   }
 
